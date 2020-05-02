5d55b28c5a64125c04b59eec0c49a0a47c0f1f46
Vivian Phu <viviana.phu@gmail.com>
2020-05-02 13:07:49 -0700

    can count prime numbers

--- app.js
+++ app.js
@@ -11,7 +11,50 @@ function primeNum(){
         document.getElementById("msg").innerHTML = "";
     }
 }
+function isPrime(num){
+    var res = true;
+    for (var i=2; i<=Math.ceil(num/2); i++){
+        if((num%i)==0){
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
 function calculatePrimeNum(firstNum,secondNum){
-    alert("calculating");
-    
+    //alert("calculating");
+    var pNumArray = [];
+    var sNum = 0;
+    var bNum = 0;
+
+
+    if (firstNum > secondNum){
+        sNum = secondNum;
+        bNum = firstNum;
+    }
+    else if (firstNum < secondNum){
+        sNum = firstNum;
+        bNum = secondNum;
+    }
+
+    console.log(sNum);
+    console.log(bNum);
+
+    while(sNum <= bNum){
+        console.log("in while");
+
+        console.log(isPrime(3));
+        if(isPrime(sNum) == true){
+            console.log(sNum);
+            pNumArray.push(sNum);
+            sNum = sNum + 1;
+        }
+        else if (isPrime(sNum) == false){
+            console.log(sNum);
+            sNum = sNum + 1;
+        }
+    }
+    console.log(pNumArray);
+    document.getElementById("primeNumber").innerHTML = "There are " + pNumArray.length +" prime numbers.";
+    document.getElementById("msg").innterHTML = pNumArray;
 }
